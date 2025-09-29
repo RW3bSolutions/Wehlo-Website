8862a4cf8f0eb501057abda1830787aeb69bb943
RW3bSolutions <richard@rweb.solutions>
2025-09-29 09:31:11 +0800

    LTS File

--- resources/js/Components/ApplicationLogo.jsx
+++
@@ -1,9 +0,0 @@
-export default function ApplicationLogo(props) {
-    return (
-        <img
-            className="object-contain size-[6rem]"
-            src="/images/Homepage/logo header.png"
-            alt=""
-        />
-    );
-}
